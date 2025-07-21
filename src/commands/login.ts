050babf9830c3f5a8ea8777313b0d03a50d504dc
Dmitri Dmitriev <d.dzmitryeu@gmail.com>
2025-07-21 21:23:47 +0300

    upd

--- src/commands/login.ts
+++ src/commands/login.ts
@@ -1,6 +1,14 @@
+import { CommandsRegistry } from "src/commands"
+import process from "node:process"
 
-export function commandLogin(argv: string[]) {
-  argv.forEach((val, index) => {
-    console.log(`${index}: ${val}`);
-  });
+export function commandLogin(commandRegistry: CommandsRegistry, argv: string[]) {
+  const args = argv.slice(2)
+  if (args.length === 0) {
+    console.log("Not enough arguments provided")
+    process.exit(1)
+  } else if (args.length === 1) {
+    console.log("Username is required")
+    process.exit(1)
+  }
+  console.log("good enough")
 }
